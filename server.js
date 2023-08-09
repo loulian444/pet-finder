1e3256c9e49d9e6468146771e241bd5ad5317986
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-08-09 14:41:26 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// write basic express framework from npm
+
+// import pets array
+
+// write get home page but keep it empty for now
+
+// write get for /pets
+  // loop through array and send
+
+// write get for /pets/:name
+  // get the name from param
+  // loop through array with if statement to find a match
+
+// write get for /pets/owner
+  // get the owner's name using query
+  // loop through array with if statement to find a match
